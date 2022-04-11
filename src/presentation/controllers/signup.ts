159e8a712280f91da5b554440ce9c4678fad841a
lucasliborio <lucasl.troncoso@gmail.com>
2022-04-11 18:46:10 -0300

    feat: signup return 400 if no email is provided

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -1,8 +1,16 @@
 export class SignUpController {
   handle (req: any): any {
-    return {
-      statusCode: 400,
-      body: new Error('missing param: name')
+    if (!req.body.name) {
+      return {
+        statusCode: 400,
+        body: new Error('missing param: name')
+      }
+    }
+    if (!req.body.email) {
+      return {
+        statusCode: 400,
+        body: new Error('missing param: email')
+      }
     }
   }
 }
