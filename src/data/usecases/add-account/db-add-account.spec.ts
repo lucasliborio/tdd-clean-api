dec313894a40c3cfbb62d7c8ad8ceb429b6835ca
lucasliborio <lucasl.troncoso@gmail.com>
2022-04-12 18:35:19 -0300

    refactor: move sut creation to an factoty helper method

--- src/data/usecases/add-account/db-add-account.spec.ts
+++ src/data/usecases/add-account/db-add-account.spec.ts
@@ -1,13 +1,26 @@
+import { Encrypter } from '../../protocols/encrypter'
 import { DbAddAccount } from './db-add-account'
-describe('DbAddAccount UseCase', () => {
+
+export interface SubType {
+  sut: DbAddAccount
+  encrypterStub: Encrypter
+}
+const makeSut = (): SubType => {
   class EncrypterStub {
     async encrypt (value: string): Promise<string> {
       return new Promise(resolve => resolve('hashed_value'))
     }
   }
+  const encrypterStub = new EncrypterStub()
+  const sut = new DbAddAccount(encrypterStub)
+  return {
+    sut,
+    encrypterStub
+  }
+}
+describe('DbAddAccount UseCase', () => {
   test('Should call Encrypter with correct password', async () => {
-    const encrypterStub = new EncrypterStub()
-    const sut = new DbAddAccount(encrypterStub)
+    const { sut, encrypterStub } = makeSut()
     const encryptspy = jest.spyOn(encrypterStub, 'encrypt')
     const accountData = {
       name: 'valid_name',
