1a4ea208a40f4456e3cde180764c978cb8f6f4b0
lucasliborio <lucasl.troncoso@gmail.com>
2022-04-18 13:54:33 -0300

    test: ensure SignUpController returns 400 if Validation fails

--- src/presentation/controllers/signup/signup.spec.ts
+++ src/presentation/controllers/signup/signup.spec.ts
@@ -194,4 +194,11 @@ describe('SignUp Controller', () => {
     await sut.handle(makeHttpRequest())
     expect(validateSpy).toHaveBeenCalledWith(makeHttpRequest().body)
   })
+
+  test('Should return 400 if dependency validation returs an error', async () => {
+    const { sut, validationStub } = makeSut()
+    jest.spyOn(validationStub, 'validate').mockReturnValueOnce(new Error())
+    const httpResponse = await sut.handle(makeHttpRequest())
+    expect(httpResponse).toEqual(badRequest(new Error()))
+  })
 })
