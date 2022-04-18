c3d5e74ede15df35d6506a028005504fd2b82092
lucasliborio <lucasl.troncoso@gmail.com>
2022-04-18 11:33:07 -0300

    feat: ensure login controller return 400 with no password is provided

--- src/presentation/controllers/login/login.spec.ts
+++ src/presentation/controllers/login/login.spec.ts
@@ -1,5 +1,5 @@
 import { MissingParamError } from '../../errors'
-import { badRequest } from '../../helpers/http-helper'
+import { badRequest, ok } from '../../helpers/http-helper'
 import { HttpRequest } from '../../protocols'
 import { LoginController } from './login'
 
@@ -7,13 +7,42 @@ describe('Login Controller', () => {
   const makeFakeRequest = (): HttpRequest => {
     return {
       body: {
+        email: 'valid email',
         password: 'valid_password'
       }
     }
   }
   test('should return 400 if no email is provided', async () => {
     const sut = new LoginController()
-    const httpResponse = await sut.handle(makeFakeRequest())
+    const httpRequestWithoutEmail = {
+      body: {
+        password: 'any_password'
+      }
+    }
+    const httpResponse = await sut.handle(httpRequestWithoutEmail)
     expect(httpResponse).toEqual(badRequest(new MissingParamError('email')))
+  })
+
+  test('should return 400 if no email is provided', async () => {
+    const sut = new LoginController()
+    const httpRequestWithoutPassword = {
+      body: {
+        email: 'any_email'
+      }
+    }
+    const httpResponse = await sut.handle(httpRequestWithoutPassword)
+    expect(httpResponse).toEqual(badRequest(new MissingParamError('password')))
+  })
+
+  test('should return 200 on valid request params', async () => {
+    const sut = new LoginController()
+    const httpResponse = await sut.handle(makeFakeRequest())
+    expect(httpResponse).toEqual(ok({ sucess: 'sucess' }))
+  })
+
+  test('should return 200 on valid request params', async () => {
+    const sut = new LoginController()
+    const httpResponse = await sut.handle(makeFakeRequest())
+    expect(httpResponse).toEqual(ok({ sucess: 'sucess' }))
   })
 })
--- src/presentation/controllers/login/login.ts
+++ src/presentation/controllers/login/login.ts
@@ -1,9 +1,15 @@
 import { MissingParamError } from '../../errors'
-import { badRequest } from '../../helpers/http-helper'
+import { badRequest, ok } from '../../helpers/http-helper'
 import { Controller, HttpRequest, HttpResponse } from '../../protocols'
 
 export class LoginController implements Controller {
   async handle (req: HttpRequest): Promise<HttpResponse> {
-    return badRequest(new MissingParamError('email'))
+    const requiredFields = ['email', 'password']
+    for (const field of requiredFields) {
+      if (!req.body[field]) {
+        return badRequest(new MissingParamError(field))
+      }
+    }
+    return ok({ sucess: 'sucess' })
   }
 }
