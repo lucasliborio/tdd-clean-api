2454259716502edb87e21adc85e8f996b3299ab8
lucasliborio <lucasl.troncoso@gmail.com>
2022-04-19 11:09:23 -0300

    refactor: move validation interface to protocols folder

--- src/main/factories/signup/signup-validation-factory.spec.ts
+++ src/main/factories/signup/signup-validation-factory.spec.ts
@@ -1,5 +1,5 @@
 import { makeSignupValidation } from './signup-validtion-factory'
-import { Validation } from '../../../presentation/helpers/validators/validation'
+import { Validation } from '../../../presentation/protocols/validation'
 import { RequiredFieldsValidation } from '../../../presentation/helpers/validators/required-field-validation'
 import { ValidationComposite } from '../../../presentation/helpers/validators/validation-composite'
 import { CompareFieldsValidation } from '../../../presentation/helpers/validators/compare-fields-validation'
--- src/presentation/controllers/login/login-protocols.ts
+++ src/presentation/controllers/login/login-protocols.ts
@@ -1,4 +1,4 @@
 export * from '../../../domain/models/account'
 export * from '../../protocols/'
 export * from '../../../domain/usecases/authentication'
-export * from '../../helpers/validators/validation'
+export * from '../../protocols/validation'
