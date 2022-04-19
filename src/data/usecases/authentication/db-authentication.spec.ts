2db87f8b7102d463fd9ca7ddb6be8ded89351de8
lucasliborio <lucasl.troncoso@gmail.com>
2022-04-19 15:30:06 -0300

    test: ensure DbAuthentication throws if LoadAccountByEmail throws

--- src/data/usecases/authentication/db-authentication.spec.ts
+++ src/data/usecases/authentication/db-authentication.spec.ts
@@ -1,7 +1,7 @@
 import { AccountModel } from '../add-account/db-add-account-protocols'
 import { LoadAccountByEmailRepository } from '../../protocols/load-account-by-email-repository'
 import { DbAuthentication } from './db-authentication'
-import { Authentication } from '../../../domain/usecases/authentication'
+import { Authentication, AuthenticationModel } from '../../../domain/usecases/authentication'
 
 interface SutTypes {
   sut: Authentication
@@ -11,6 +11,12 @@ const makeAccount = (): AccountModel => {
   return {
     id: 'valid_id',
     name: 'valid_name',
+    email: 'valid_email',
+    password: 'valid_password'
+  }
+}
+const makeFakeAuthenticationModel = (): AuthenticationModel => {
+  return {
     email: 'valid_email',
     password: 'valid_password'
   }
@@ -36,10 +42,14 @@ describe('', () => {
   test('Should call LoadAccountByEmailrepository with correct email', async () => {
     const { sut, loadAccountByEmailRepositoryStub } = makeSut()
     const loadSpy = jest.spyOn(loadAccountByEmailRepositoryStub, 'load')
-    await sut.auth({
-      email: 'any_email@gmail.com',
-      password: 'any_password'
-    })
-    expect(loadSpy).toHaveBeenCalledWith('any_email@gmail.com')
+    await sut.auth(makeFakeAuthenticationModel())
+    expect(loadSpy).toHaveBeenCalledWith('valid_email')
+  })
+
+  test('Should throw if LoadAccountByEmailRepository Thorws', async () => {
+    const { sut, loadAccountByEmailRepositoryStub } = makeSut()
+    jest.spyOn(loadAccountByEmailRepositoryStub, 'load').mockRejectedValueOnce(new Error())
+    const promise = sut.auth(makeFakeAuthenticationModel())
+    await expect(promise).rejects.toThrow()
   })
 })
