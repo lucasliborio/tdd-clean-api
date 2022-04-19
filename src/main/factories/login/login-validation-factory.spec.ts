ffaa223b117be9c6b3fdfec81906df5d6af17690
lucasliborio <lucasl.troncoso@gmail.com>
2022-04-19 11:08:27 -0300

    refactor: move http-helper to a folder

--- src/main/factories/login/login-validation-factory.spec.ts
+++ src/main/factories/login/login-validation-factory.spec.ts
@@ -2,18 +2,10 @@ import { Validation } from '../../../presentation/helpers/validators/validation'
 import { RequiredFieldsValidation } from '../../../presentation/helpers/validators/required-field-validation'
 import { ValidationComposite } from '../../../presentation/helpers/validators/validation-composite'
 import { EmailValidation } from '../../../presentation/helpers/validators/email-validation'
-import { EmailValidator } from '../../../presentation/protocols'
 import { makeLoginValidation } from './login-validation-factory'
+import { EmailValidatorAdapter } from '../../../util/email-validator-adapter'
 
 jest.mock('../../../presentation/helpers/validators/validation-composite')
-const makeEmailValidator = (): EmailValidator => {
-  class EmailValidatorStub implements EmailValidator {
-    isValid (email: string): boolean {
-      return true
-    }
-  }
-  return new EmailValidatorStub()
-}
 
 describe('SignUpValidation Factory', () => {
   test('Should call ValidationComposite with all validations', () => {
@@ -22,7 +14,7 @@ describe('SignUpValidation Factory', () => {
     for (const fiels of ['email', 'password']) {
       validations.push(new RequiredFieldsValidation(fiels))
     }
-    validations.push(new EmailValidation('email', makeEmailValidator()))
+    validations.push(new EmailValidation('email', new EmailValidatorAdapter()))
     expect(ValidationComposite).toHaveBeenCalledWith(validations)
   })
 })
--- src/presentation/controllers/signup/signup.spec.ts
+++ src/presentation/controllers/signup/signup.spec.ts
@@ -1,7 +1,7 @@
 import { InvalidParamError, ServerError } from '../../errors'
 import { AddAccount, AddAccountModel, AccountModel, HttpRequest, Validation } from './signup-protocols'
 import { SignUpController } from './signup'
-import { ok, serverError, badRequest } from '../../helpers/http-helper'
+import { ok, serverError, badRequest } from '../../helpers/http/http-helper'
 
 /* const makeEmailValidatorWithError = (): EmailValidator => {
   class EmailValidatorStub implements EmailValidator {
