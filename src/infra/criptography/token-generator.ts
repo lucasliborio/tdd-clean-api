a89a9ec5b73ff952dabc54d18791366c999110dc
lucasliborio <lucasl.troncoso@gmail.com>
2022-04-19 18:34:24 -0300

    feat:crete tokenGenerator Dependency

--- src/infra/criptography/token-generator.ts
+++ src/infra/criptography/token-generator.ts
@@ -1,7 +1,8 @@
-import { TokenGenerator } from '../../data/protocols/criptography/token-generator';
+import { TokenGenerator } from '../../data/protocols/criptography/token-generator'
+import jwt from 'jsonwebtoken'
 
 export class TokenGeneratorAdapter implements TokenGenerator {
   async generate (id: string): Promise<string> {
-
+    const validToken = await jwt.sign(id)
   }
 }
