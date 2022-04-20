9954634cfaff39cdc3cbb04bb130d0add9fd73c5
lucasliborio <lucasl.troncoso@gmail.com>
2022-04-20 12:32:22 -0300

    refactor: remove bcrypt adapter to a folder

--- src/infra/criptography/jwt-adapter.ts
+++ src/infra/criptography/jwt-adapter.ts
@@ -1,7 +1,7 @@
 import { Encrypter } from '../../data/protocols/criptography/encrypter'
 import jwt from 'jsonwebtoken'
 
-export class EncrypterAdapter implements Encrypter {
+export class JwtAdapter implements Encrypter {
   async encrypt (value: string): Promise<string> {
     return new Promise((resolve, reject) => {
       jwt.sign(value, 'dfsdhfoisdhfodshfjoisdfsf', (err, decoded) => {
