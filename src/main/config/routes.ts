bc6c516c511b2a4c3c40976530c03ebaf40c1700
lucasliborio <lucasl.troncoso@gmail.com>
2022-04-23 15:07:13 -0300

    refactor: change routes to use filysystem

--- src/main/config/routes.ts
+++ src/main/config/routes.ts
@@ -6,7 +6,7 @@ const router = Router()
 
 export default (app: Express): void => {
   app.use('/api', router)
-  readdirSync(path.resolve('src/main/routes')).filter(x => x.includes('routes') && !x.includes('test')).map(async file => {
-    await import(`../routes/${file}`).then(modulo => modulo.default(router))
+  readdirSync(path.resolve(__dirname, '../routes')).filter(x => x.includes('routes') && !x.includes('test')).map(async file => {
+    (await import(`../routes/${file}`)).default(router)
   })
 }
