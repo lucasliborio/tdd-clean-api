2fc04ee9bfcbcefc4458bb35edd113fee0f50ac8
lucasliborio <lucasl.troncoso@gmail.com>
2022-04-23 15:56:57 -0300

    refactor: refactor process env

--- src/main/env.ts
+++ src/main/env.ts
@@ -1,5 +1,5 @@
 export default {
   MongoUrl: 'mongodb+srv://lucasdev:lucasdevproj@cluster0.ekpwh.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
-  port: 3000,
+  port: process.env.PORT || 3000,
   secret: 'fdshfdofjidsofinsofinsdofisnd'
 }
