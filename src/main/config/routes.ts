568a5a7d8027e4839bfb288dd4b9967a704681f5
lucasliborio <lucasl.troncoso@gmail.com>
2022-04-27 14:19:34 -0300

    chore: enable debug with vscode

--- src/main/config/routes.ts
+++ src/main/config/routes.ts
@@ -6,7 +6,7 @@ const router = Router()
 
 export default (app: Express): void => {
   app.use('/api', router)
-  readdirSync(path.resolve(__dirname, '../routes')).filter(x => x.includes('routes') && !x.includes('test')).map(async file => {
+  readdirSync(path.resolve(__dirname, '../routes')).filter(x => x.includes('routes') && !x.includes('test') && !x.includes('map')).map(async file => {
     (await import(`../routes/${file}`)).default(router)
   })
 }
