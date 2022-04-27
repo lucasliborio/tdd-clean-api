66f5282d4d9d3dd2b4a1d6474e5a5a2e38fbd1eb
lucasliborio <lucasl.troncoso@gmail.com>
2022-04-27 10:58:19 -0300

    chore: add docker-compose

--- Dockerfile
+++ Dockerfile
@@ -2,6 +2,4 @@ FROM node:16
 WORKDIR /usr/workspace/tdd-node-api
 COPY ./package.json .
 RUN npm install --only=prod
-COPY ./dist ./dist
-EXPOSE 5000
-CMD npm start
+
--- src/main/env.ts
+++ src/main/env.ts
@@ -1,5 +1,5 @@
 export default {
-  MongoUrl: process.env.MONGO_URL || 'mongodb+srv://lucasdev:lucasdevproj@cluster0.ekpwh.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
-  port: process.env.PORT || 3000,
+  MongoUrl: process.env.MONGO_URL || 'mongodb://mongo:27017/clean-node-api' || 'mongodb+srv://lucasdev:lucasdevproj@cluster0.ekpwh.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+  port: process.env.PORT || 5050,
   secret: 'fdshfdofjidsofinsofinsdofisnd'
 }
