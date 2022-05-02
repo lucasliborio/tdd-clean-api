e30d08148df442100af4b62c0f13a075d1010961
lucasliborio <lucasl.troncoso@gmail.com>
2022-05-02 18:47:26 -0300

    chore:remove src/data from git and eslint ignore

---
+++ src/main/factories/usecases/add-account/db-add-account-factory.ts
@@ -0,0 +1,10 @@
+import { DbAddAccount } from '../../../../data/usecases/add-account/db-add-account'
+import { BcryptAdapter } from '../../../../infra/criptography/bcrypt-adapter/bcrypt-adapter'
+import { AccountMongoRepository } from '../../../../infra/db/mongodb/account/account-mongo-repository'
+
+export const makeDbAddAccountFactory = (): DbAddAccount => {
+  const salt = 12
+  const bcrypAdapter = new BcryptAdapter(salt)
+  const accountMongoRepository = new AccountMongoRepository()
+  return new DbAddAccount(bcrypAdapter, accountMongoRepository)
+}
