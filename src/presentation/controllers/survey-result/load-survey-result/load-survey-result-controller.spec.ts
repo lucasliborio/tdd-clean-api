b3a552639594801f56e26bba5478c9ac2c6d140c
lucasliborio <lucasl.troncoso@gmail.com>
2022-06-01 17:09:32 -0300

    feat: ensure that LoadSurveyResult call LoadSurveyResult with correct values

--- src/presentation/controllers/survey-result/load-survey-result/load-survey-result-controller.spec.ts
+++ src/presentation/controllers/survey-result/load-survey-result/load-survey-result-controller.spec.ts
@@ -1,6 +1,8 @@
+import { SurveyResultModel } from '@/domain/models/survey-result'
+import { LoadSurveyResult } from '@/domain/usecases/survey-result/load-survey-result'
 import { LoadSurveyById } from '@/domain/usecases/survey/load-survey-by-id'
 import { InvalidParamError } from '@/presentation/errors'
-import { forbidden } from '@/presentation/helpers/http/http-helper'
+import { forbidden, serverError } from '@/presentation/helpers/http/http-helper'
 import { Controller, HttpRequest, SurveyModel } from '../../survey/load-survey/load-survey-protocols'
 import { LoadSurveyResultController } from './load-survey-result-controller'
 
@@ -11,23 +13,64 @@ const mockFakeRequest = (): HttpRequest => {
     }
   }
 }
+const mockFakeSurvey = (): SurveyModel => {
+  return {
+    id: 'valid_id',
+    question: 'what tech do you know?',
+    answers: [{
+      answer: 'node'
+    },
+    {
+      answer: 'python'
+    },
+    {
+      answer: 'goolang'
+    }],
+    date: new Date()
+  }
+}
+const mockSurveyResult = (): SurveyResultModel => ({
+  surveyId: 'valid_survey_id',
+  question: 'langueage most used',
+  answers: [{
+    answer: 'node'
+  },
+  {
+    answer: 'python'
+  },
+  {
+    answer: 'goolang'
+  }],
+  date: new Date()
+})
+
 const mockLoadSurveyById = (): LoadSurveyById => {
   class LoadSurveyByIdStub implements LoadSurveyById {
     async load (id: string): Promise<SurveyModel> {
-      return Promise.resolve(null)
+      return Promise.resolve(mockFakeSurvey())
     }
   }
   return new LoadSurveyByIdStub()
 }
+const mockLoadSurveyResult = (): LoadSurveyResult => {
+  class LoadSurveyResultStub implements LoadSurveyResult {
+    async load (id: string): Promise<SurveyResultModel> {
+      return Promise.resolve(mockSurveyResult())
+    }
+  }
+  return new LoadSurveyResultStub()
+}
 interface SutTypes {
   sut: Controller
   loadSurveyByIdStub: LoadSurveyById
+  loadSurveyResultStub: LoadSurveyResult
 }
 const makeSut = (): SutTypes => {
   const loadSurveyByIdStub = mockLoadSurveyById()
-  const sut = new LoadSurveyResultController(loadSurveyByIdStub)
+  const loadSurveyResultStub = mockLoadSurveyResult()
+  const sut = new LoadSurveyResultController(loadSurveyByIdStub, loadSurveyResultStub)
   return {
-    sut, loadSurveyByIdStub
+    sut, loadSurveyByIdStub, loadSurveyResultStub
   }
 }
 describe('LoadSurveyResult Controller', () => {
@@ -42,5 +85,17 @@ describe('LoadSurveyResult Controller', () => {
     jest.spyOn(loadSurveyByIdStub, 'load').mockResolvedValueOnce(null)
     const result = await sut.handle(mockFakeRequest())
     expect(result).toEqual(forbidden(new InvalidParamError('surveyId')))
+  })
+  test('Should return 500 if LoadSurveyById throws', async () => {
+    const { sut, loadSurveyByIdStub } = makeSut()
+    jest.spyOn(loadSurveyByIdStub, 'load').mockRejectedValueOnce(new Error())
+    const result = await sut.handle(mockFakeRequest())
+    expect(result).toEqual(serverError(new Error()))
+  })
+  test('Should call loadSurveyResult with correctValues', async () => {
+    const { sut, loadSurveyResultStub } = makeSut()
+    const loadResultSpy = jest.spyOn(loadSurveyResultStub, 'load')
+    await sut.handle(mockFakeRequest())
+    expect(loadResultSpy).toBeCalledWith('any_survey_id')
   })
 })
--- src/presentation/controllers/survey-result/load-survey-result/load-survey-result-controller.ts
+++ src/presentation/controllers/survey-result/load-survey-result/load-survey-result-controller.ts
@@ -1,16 +1,26 @@
+import { LoadSurveyResult } from '@/domain/usecases/survey-result/load-survey-result'
 import { LoadSurveyById } from '@/domain/usecases/survey/load-survey-by-id'
 import { InvalidParamError } from '@/presentation/errors'
-import { forbidden } from '@/presentation/helpers/http/http-helper'
+import { forbidden, ok, serverError } from '@/presentation/helpers/http/http-helper'
 import { Controller, HttpRequest, HttpResponse } from '../../survey/load-survey/load-survey-protocols'
 
 export class LoadSurveyResultController implements Controller {
   constructor (
-    private readonly loadSurveyById: LoadSurveyById
+    private readonly loadSurveyById: LoadSurveyById,
+    private readonly loadSurveyResult: LoadSurveyResult
   ) {}
 
   async handle (req: HttpRequest): Promise<HttpResponse> {
-    const surveyId = req.params.surveyId
-    const surveyById = await this.loadSurveyById.load(surveyId)
-    if (!surveyById) return forbidden(new InvalidParamError('surveyId'))
+    try {
+      const surveyId = req.params.surveyId
+      const surveyById = await this.loadSurveyById.load(surveyId)
+      if (!surveyById) {
+        return forbidden(new InvalidParamError('surveyId'))
+      }
+      const loadSurveyResult = await this.loadSurveyResult.load(surveyId)
+      return ok(loadSurveyResult)
+    } catch (error) {
+      return serverError(error)
+    }
   }
 }
